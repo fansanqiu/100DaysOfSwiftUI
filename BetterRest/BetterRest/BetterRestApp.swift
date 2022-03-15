a7e77b54b52cc00d438de0573c46a38fe249ed71
范三球 <fansanqiu@gmail.com>
2022-03-15 16:53:08 +0800

    新增BetterRest项目

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by 范三球 on 2022/3/15.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
